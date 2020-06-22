ee1481fde16b747ba40b5b1581a0587d3a4cbc6e
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-06-21 23:45:18 -0400

    Refactor settings

--- frontend/src/custom_axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const REST_URL = process.env.VUE_APP_REST_API
-
-export default axios.create({
-    baseURL: REST_URL,
-    withCredentials: true,
-})
--- frontend/vue.config.js
+++
@@ -1,9 +0,0 @@
-process.env.VUE_APP_REST_API = process.env.NODE_ENV === 'production' ?
-    process.env.REST_API_URL : 'http://localhost:8000/api/v1.0/';
-
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? process.env.VUE_PUBLIC_PATH // URL for production server
-        : '/',
-        assetsDir: 'static'
-};
