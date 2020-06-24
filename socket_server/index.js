c8ee09bf9190ae82e8f8b1a17f9dad5051685145
mvargas33 <mvargas33@protonmail.com>
2020-06-24 18:06:56 -0400

    feat(socket-back-server): Creado servidor central para concurrencia

---
+++ socket_server/index.js
@@ -0,0 +1,26 @@
+'use strict' // Para usar nueva notación de JS
+
+const bodyParser = require('body-parser'); // Para parsear solicitudes POST y otras
+const app = require('express')() // Servidor
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 3000 // Puerto del servidor
+
+app.use(bodyParser.urlencoded({extended: true })) // Estas dos líneas son estándar
+app.use(bodyParser.json()) // Usar formato JSON para POST
+
+server.listen(port, () => { // Pasamos una lambda que solo hace log
+    console.log(`Servidor corriendo en http://localhost:${port}`) // Print inicial del servidor
+})
+
+io.on('connection', function(socket) {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    })
+
+    io.emit('new_user', {user: nuevoUser});
+})
+
