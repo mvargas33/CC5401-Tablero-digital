a71e37520e71df474ca079a0fd847b294b8e4213
GMondaca <elbailedelmono@gmail.com>
2020-06-24 01:15:03 -0400

    fix (config): Fix errors for production configs
    In a previous commit configuration was refactored. Database config was erroneously defined on development settings only, instead of base. Also, changed wsgi.py to use the appropriate production configs.

--- tablero_digital/wsgi.py
+++ tablero_digital/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tablero_digital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tablero_digital.settings.production')
 
 application = get_wsgi_application()
