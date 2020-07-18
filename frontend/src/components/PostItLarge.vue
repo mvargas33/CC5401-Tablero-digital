da3a2ef5c7ba7a025985a96aec0b0b9d51e38b77
StarBrand <juan.saez.hidalgo@gmail.com>
2020-07-18 18:36:07 -0400

    Trying to add Icons

--- frontend/src/components/PostItLarge.vue
+++ frontend/src/components/PostItLarge.vue
@@ -6,11 +6,19 @@
   >
     <h3 class="">{{ title }}</h3>
     <p class="post-it-description p-0">{{ description }}</p>
+    <ul class="concurrent-container">
+      <user-icon-large
+              v-for="user in postit.concurrent_users"
+              :key="user.id"
+              :user_icon="user"
+      />
+    </ul>
   </li>
 
 </template>
 
 <script>
+  import UserIconLarge from "./UserIconLarge";
 
 function textEllipsis(text, length){
   // Cuts text and appends ellipsis if text.length is greater than length.
@@ -23,6 +31,18 @@ function textEllipsis(text, length){
 
 export default {
   name: "PostItLarge",
+  components: {
+    UserIconLarge
+  },
+  data(){
+    return {
+      concurrent_users: [
+        {
+          name: "Juan"
+        }
+      ]
+    }
+  },
   props: ["postit"],
   computed: {
     title(){
@@ -36,20 +56,28 @@ export default {
 </script>
 
 <style>
-/* Post-it colors where defined in PostItSmall.vue */
+  /* Post-it colors where defined in PostItSmall.vue */
 
-.post-it-large{
-  cursor: pointer;
-  max-width: 20rem;
-  min-width: 15rem;
-  min-height: 15rem;
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.4);
-  border-radius: 3px;
-  list-style: none;
-}
+  .post-it-large{
+    cursor: pointer;
+    max-width: 20rem;
+    min-width: 15rem;
+    min-height: 15rem;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.4);
+    border-radius: 3px;
+    list-style: none;
+  }
 
-.post-it-description{
-  font-size: 1.2rem;
-}
+  .post-it-description{
+    font-size: 1.2rem;
+  }
+
+  .concurrent-container {
+    width: 100%;
+    padding-left: 0.5rem;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+  }
 
 </style>
---
+++ frontend/src/components/UserIconLarge.vue
@@ -0,0 +1,46 @@
+<template>
+  <li
+          class="user-icon-large"
+  >
+    <h3 class="">{{ letter }}</h3>
+  </li>
+
+</template>
+
+<script>
+export default {
+  name: "UserIconLarge",
+  props: ["user_icon"],
+  computed: {
+    letter(){
+      return this.user_icon.name[0];
+    }
+  }
+};
+</script>
+
+<style>
+  /* Post-it colors where defined in PostItSmall.vue */
+
+  .user-icon-large{
+    position: relative;
+    list-style: none;
+    color: white;
+    text-align: center;
+    max-width: 10rem;
+    max-height: 6rem;
+    margin-right: 1rem;
+    margin-bottom: 1.2rem;
+    padding: 0.7rem 1rem;
+    border-radius: 2px;
+    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
+    cursor: pointer;
+    transition-property: transform, filter;
+    transition-duration:  0.15s;
+    filter: brightness(1);
+    transform: rotate(2deg);
+    z-index: 0;
+    font-weight: 500;
+  }
+
+</style>
